f2079cb1893f92b4d8c76cc963bc1d65bf5564d3
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-10-09 21:41:16 +0700

    feat: allow service binding rpc

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
+import { WorkerEntrypoint } from "cloudflare:workers";
+import { getNotifierHandlerForUid } from "./app/load-balancer";
 import { Notifier } from "./app/notifier";
 import { router } from "./app/routes";
 
-export default {
-	async fetch(request, env, context) {
-		return await router.handle(request);
+export default class SSEWorker extends WorkerEntrypoint {
+	fetch(request: Request): Response | Promise<Response> {
+		return router.handle(request);
 	}
-} satisfies ExportedHandler<Env>;
+
+	sendEvent(uid: string, data: string) {
+		const notifier = getNotifierHandlerForUid(uid);
+		return notifier.sendEvent(uid, data);
+	}
+};
 
 export { Notifier };
 
