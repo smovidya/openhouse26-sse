653513e8fc6689c16289c4c0215de77f3830f0b8
ongsalt <ongsa.srp.mkm@gmail.com>
2025-10-12 00:40:26 +0700

    bruh

--- src/app/feature-flag.ts
+++ src/app/feature-flag.ts
@@ -2,7 +2,7 @@ import { env } from "cloudflare:workers";
 
 export const dev = env.ENVIRONMENT === "development";
 export const staging = env.ENVIRONMENT === "staging";
-export const production = env.ENVIRONMENT !== "production";
+export const production = env.ENVIRONMENT === "production";
 export const notProduction = !production;
 
 export const flags = {
