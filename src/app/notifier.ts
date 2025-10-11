5bdd5599953f2e346c3bf4e2f87596e01d6752be
ongsalt <ongsa.srp.mkm@gmail.com>
2025-10-11 21:46:50 +0700

    feat: update auth process

--- src/app/notifier.ts
+++ src/app/notifier.ts
@@ -6,23 +6,37 @@ export class Notifier extends DurableObject<Env> {
 	#wss = new MultiMap<string, WebSocket>();
 
 	// idk if i need to name this `fetch` or not
-	createConnection(uid: string): Response {
-		const connections = this.#wss.get(uid);
-		if (connections.length >= 5) {
-			return new Response("too many connections", {
-				status: 429
-			});
-		}
-
+	createConnection(): Response {
 		const webSocketPair = new WebSocketPair();
 		const [client, server] = Object.values(webSocketPair);
 		server.accept();
-		this.#wss.add(uid, client);
 
-		server.addEventListener("close", (cls) => {
-			this.#wss.remove(uid, client);
-			server.close(cls.code, "Durable Object is closing WebSocket");
-		});
+		// close if no jwt are sent in 1 sec
+		const id = setTimeout(() => {
+			server.close();
+		}, 1000);
+
+		const persistConnection = (uid: string) => {
+			clearTimeout(id);
+			const connections = this.#wss.get(uid);
+			if (connections.length >= 5) {
+				return new Response("too many connections", {
+					status: 429
+				});
+			}
+
+			this.#wss.add(uid, client);
+
+			server.addEventListener("close", (cls) => {
+				this.#wss.remove(uid, client);
+			});
+		}
+
+		server.addEventListener("message", ({ data }) => {
+			// const uid = "TODO: verify jwt on first message, ignore subsequent";
+			const uid = "isei";
+			persistConnection(uid);
+		}, { once: true })
 
 		return new Response(null, {
 			status: 101,
